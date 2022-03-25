de58aed8404639dde3a3795671e15394fed9c4c6
andev240 <umesh.patidar@mosaicwellness.in>
2022-03-25 16:53:57 +0530

    Ral fn invoke

--- App.tsx
+++ App.tsx
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { View,Text,Button } from 'react-native';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import {useAddress} from '@mosaic-wellness/redux-action-library'
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+const {getAddresses} = useAddress();
 
+  const x = ()=>{
+    const result = getAddresses();
+    console.log(result,"Add");
+    
+  }
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <View>
+          <Text>Dummy Testing</Text>
+          <Button title='Click Me' onPress={x}/> 
+      </View>
+      // <SafeAreaProvider>
+      //   <Navigation colorScheme={colorScheme} />
+      //   <StatusBar />
+      // </SafeAreaProvider>
     );
   }
 }
